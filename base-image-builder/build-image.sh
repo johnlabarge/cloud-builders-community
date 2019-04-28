08de19f7b52ca20a064f48a64d11200a16a7dc90
Karim Elatov <elatov@google.com>
2019-04-28 01:28:23 -0400

    add draft of readme and add new substitution variables for the build

--- base-image-builder/build-image.sh
+++ base-image-builder/build-image.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-AGENT_IMAGE='packer-nested-virtualization-agent'
+AGENT_IMAGE='nested-packer'
 USERNAME=${USERNAME:-admin}
 REMOTE_WORKSPACE=${REMOTE_WORKSPACE:-/home/${USERNAME}/workspace/}
 INSTANCE_NAME=${INSTANCE_NAME:-builder-$(cat /proc/sys/kernel/random/uuid)}
-ZONE=${ZONE:-us-central1-f}
+IMAGE_ZONE=${IMAGE_ZONE:-us-central1-f}
 INSTANCE_ARGS=${INSTANCE_ARGS:---preemptible}
-gcloud config set compute/zone ${ZONE}
+gcloud config set compute/zone ${IMAGE_ZONE}
 
 function cleanup {
     gcloud compute instances delete ${INSTANCE_NAME} --quiet
 }
+
+function wait_for_ssh {
+    echo "Attempting to SSH to Builder machine..."
+    attempt=1
+    while (( $attempt <= 5 )); do
+    gcloud compute ssh  --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} --ssh-flag="-Nf" && EC=$?|| EC=$? && true
+    case ${EC} in
+        (0) echo "Success after ${attempt} try"; break ;;
+        (*) echo "${attempt} of 5 failed attempts, Builder SSH Machine not ready yet, waiting 2 seconds..." ;;
+    esac
+    sleep 2s
+    ((attempt+=1))
+    done
+}
+
 mkdir -p ~/.ssh/
 touch ~/.ssh/google_compute_known_hosts
 chown -v `whoami` ~/.ssh/google_compute_known_hosts
@@ -31,15 +46,15 @@ gcloud compute instances create \
        --min-cpu-platform='Intel Haswell' \
        --scopes=storage-rw,compute-rw
 
-
 tar -czf workspace.tgz *
+
+wait_for_ssh
 
 gcloud compute scp --compress --recurse \
        workspace.tgz ${USERNAME}@${INSTANCE_NAME}:~ \
        --ssh-key-file=${KEYNAME}
 
 gcloud compute ssh --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} -- 'tar -xvzf workspace.tgz'
-
 
 gcloud compute ssh  --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} -- 'ls -l'
 COMMAND="export BUILD_NUMBER="$(echo ${TAG_NAME} | sed 's/\.//')" &&  packerio build \
@@ -49,7 +64,6 @@ COMMAND="export BUILD_NUMBER="$(echo ${TAG_NAME} | sed 's/\.//')" &&  packerio b
 -var \"zone=${IMAGE_ZONE}\" \
 ${PACKER_SPEC}"
 
-gcloud compute ssh  --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} -- $COMMAND
-
+gcloud compute ssh  --ssh-key-file=${KEYNAME} ${USERNAME}@${INSTANCE_NAME} -- ${COMMAND}
 
 trap cleanup EXIT
